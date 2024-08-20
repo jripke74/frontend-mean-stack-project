259620c4c93a50926b8bbf1973f0fd3075ec6fe5
Jeff Ripke <jripke74@gmail.com>
2024-08-20 10:07:54 -0700

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
 import Public from './components/Public.jsx';
+import Login from './components/Login.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </Routes>
   );
