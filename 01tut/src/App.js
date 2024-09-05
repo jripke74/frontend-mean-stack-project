cdd5a16c60de8fd4ac0784368566dbb05393cc05
Jeff Ripke <jripke74@gmail.com>
2024-09-05 12:34:10 -0700

    add content.jsx

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,7 +1,6 @@
 import Header from './Header.jsx';
 import Content from './Content.jsx';
 import Footer from './Footer.jsx';
-import './App.css';
 
 function App() {
 
--- 01tut/src/Content.jsx
+++ 01tut/src/Content.jsx
@@ -4,11 +4,27 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   }
+
+  const handleClick = () => {
+    console.log('You clicked it')
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
     <main>
-      <p>
-        Hello {handleNameChange()}
+      <p onDoubleClick={handleClick}>
+        Hello {handleNameChange()}!
       </p>
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2('Jeff')}>Click It</button>
+      <button onClick={(e) => handleClick3(e)}>Click It</button>
     </main>
   )
 }
