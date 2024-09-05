b587cac48cf7537690f2404a6ac9f4f948dd8dba
Jeff Ripke <jripke74@gmail.com>
2024-09-05 10:59:45 -0700

    add components

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Header from './Header.jsx';
+import Content from './Content.jsx';
+import Footer from './Footer.jsx';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
