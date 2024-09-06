46a2511c05d719e02f8ec5ed6066cc19b047531f
Jeff Ripke <jripke74@gmail.com>
2024-09-06 10:58:48 -0700

    add LineItem

--- 01tut/src/App.js
+++ 01tut/src/App.js
@@ -1,14 +1,51 @@
-import Header from './Header.jsx';
-import Content from './Content.jsx';
-import Footer from './Footer.jsx';
+import { useState } from "react";
+
+import Header from "./Header.jsx";
+import Content from "./Content.jsx";
+import Footer from "./Footer.jsx";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shippinglist", JSON.stringify(listItems));
+  };
 
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Grocerie List" />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
