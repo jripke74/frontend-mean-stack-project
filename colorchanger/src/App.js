8fd4a36c50c654e70966972361aa82ef7dd4dfce
Jeff Ripke <jripke74@gmail.com>
2024-09-09 08:26:20 -0700

    add colorValue

--- colorchanger/src/App.js
+++ colorchanger/src/App.js
@@ -1,11 +1,13 @@
 import Square from "./Square";
 import Input from "./Input";
+import { useState } from "react";
 
 function App() {
+  const [colorValue, setColorValue] = useState("");
   return (
     <div className="App">
-      <Square />
-      <Input />
+      <Square colorValue={colorValue} />
+      <Input colorValue={colorValue} setColorValue={setColorValue} />
     </div>
   );
 }
