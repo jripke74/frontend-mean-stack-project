a132383e84eab723df399ba4da5470037c5b4464
Jeff Ripke <jripke74@gmail.com>
2024-09-09 09:22:35 -0700

    add toggle button

--- colorchanger/src/Input.jsx
+++ colorchanger/src/Input.jsx
@@ -1,4 +1,12 @@
-const Input = ({ colorValue, setColorValue }) => {
+import colorNames from "colornames";
+
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <label>Add Color Name:</label>
@@ -8,8 +16,14 @@ const Input = ({ colorValue, setColorValue }) => {
         placeholder="Add color name"
         required
         value={colorValue}
-        onChange={(e) => setColorValue(e.target.value)}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
       />
+      <button type="button" onClick={() => setIsDarkText(!isDarkText)}>
+        Toggle Text Color
+      </button>
     </form>
   );
 };
