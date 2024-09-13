5470aba687e05289d1c31aeaa0960295d9b521c0
Jeff Ripke <jripke74@gmail.com>
2024-09-13 08:46:13 -0700

    add multiple files

--- reactjsblog/src/App.js
+++ reactjsblog/src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Header from "./Header.jsx";
+import Nav from "./Nav.jsx";
+import Footer from "./Footer.jsx";
+import Home from "./Home.jsx";
+import NewPost from "./NewPost.jsx";
+import PostPage from "./PostPage.jsx";
+import About from "./About.jsx";
+import Missing from "./Missing.jsx";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
