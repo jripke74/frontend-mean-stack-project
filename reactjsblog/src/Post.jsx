75f28cc8d3988bb6605a3f772a8ffe5287f58457
Jeff Ripke <jripke74@gmail.com>
2024-09-13 17:30:57 -0700

    add Post.js

--- reactjsblog/src/Post.jsx
+++ reactjsblog/src/Post.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 const Post = ({ post }) => {
   return (
     <article className="post">
-      <Link to={`/post${post.id}`}>
+      <Link to={`/post/${post.id}`}>
         <h2>{post.title}</h2>
-        <p className="postBody">
-          {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)}...`}
-        </p>
+        <p className="postDate">{post.datetime}</p>
       </Link>
+      <p className="postBody">
+        {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)}...`}
+      </p>
     </article>
   );
 };
--- reactjsblog/src/PostPage.jsx
+++ reactjsblog/src/PostPage.jsx
@@ -3,7 +3,7 @@ import { useParams, Link } from "react-router-dom";
 const PostPage = ({ posts, handleDelete }) => {
   const { id } = useParams();
   const post = posts.find((post) => post.id.toString() === id);
-
+  console.log(post.datetime);
   return (
     <main className="PostPage">
       <article className="post">
