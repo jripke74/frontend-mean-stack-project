10872645efaad7412e875aa2b49370a831291410
Jeff Ripke <jripke74@gmail.com>
2024-09-16 07:32:31 -0700

    add EditPost.jsx

---
+++ reactjsblog/src/EditPost.jsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import { useParams, link } from "react-router-dom";
+
+const EditPost = ({
+  post,
+  handleEdit,
+  editBody,
+  setEditBody,
+  editTitle,
+  setEditTitle,
+}) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+
+  useEffect(() => {
+    if (post) {
+      setEditTitle(post.titel);
+      setEditBody(post.body);
+    }
+  }, [post, setEditTitle, setEditBody]);
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          id="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
+};
+
+export default EditPost;
