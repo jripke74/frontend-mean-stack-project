4e91c7dc8ab08abd6883579c669fe14f785a39b6
Jeff Ripke <jripke74@gmail.com>
2024-09-17 10:29:20 -0700

    add DataContext

--- reactjsblog/src/PostPage.jsx
+++ reactjsblog/src/PostPage.jsx
@@ -1,6 +1,10 @@
 import { useParams, Link } from "react-router-dom";
+import { useContext } from "react";
+import DataContext from "./context/DataContext.js";
 
-const PostPage = ({ posts, handleDelete }) => {
+const PostPage = () => {
+  const { posts, handleDelete } = useContext(DataContext);
+
   const { id } = useParams();
   const post = posts.find((post) => post.id.toString() === id);
 
