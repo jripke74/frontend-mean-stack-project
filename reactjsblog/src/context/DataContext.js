08d6ac7cba646c8694b0eca622178a7103d9f07b
Jeff Ripke <jripke74@gmail.com>
2024-09-18 09:51:24 -0700

    add misc

--- reactjsblog/src/context/DataContext.js
+++ reactjsblog/src/context/DataContext.js
@@ -1,7 +1,4 @@
-import { useHistory } from "react-router-dom";
 import { createContext, useState, useEffect } from "react";
-import { format } from "date-fns";
-import api from "../api/posts.js";
 import useAxiosFetch from "../hooks/useAxiosFetch.js";
 
 const DataContext = createContext({});
@@ -10,9 +7,6 @@ export const DataProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [editTitle, setEditTitle] = useState("");
-  const [editBody, setEditBody] = useState("");
-  const history = useHistory();
 
   const { data, fetchError, isLoading } = useAxiosFetch(
     "http://localhost:3500/posts"
@@ -31,33 +25,6 @@ export const DataProvider = ({ children }) => {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/posts/${id}`);
-      const postsList = posts.filter((post) => post.id !== id);
-      setPosts(postsList);
-      history.push("/");
-    } catch (err) {
-      console.log(`Error: ${err}`);
-    }
-  };
-
-  const handleEdit = async (id) => {
-    const datetime = format(new Date(), "MMMM dd, yyyy pp");
-    const updatedPost = { id, title: editTitle, datetime, body: editBody };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(
-        posts.map((post) => (post.id === id ? { ...response.data } : post))
-      );
-      setEditTitle("");
-      setEditBody("");
-      history.push("/");
-    } catch (err) {
-      console.log(`Error: ${err.message}`);
-    }
-  };
-
   return (
     <DataContext.Provider
       value={{
@@ -68,12 +35,6 @@ export const DataProvider = ({ children }) => {
         isLoading,
         posts,
         setPosts,
-        handleEdit,
-        editBody,
-        setEditBody,
-        editTitle,
-        setEditTitle,
-        handleDelete,
       }}
     >
       {children}
