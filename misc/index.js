93fb8a57310c89c7a901dcb67d4bff15253d30c4
Jeff Ripke <jripke74@gmail.com>
2024-09-24 15:24:26 -0700

    add onclick

--- misc/index.js
+++ misc/index.js
@@ -1,16 +1,9 @@
-let count = 5;
-count = count + 1;
-console.log(count);
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-let bonusPoints = 50;
-console.log(bonusPoints);
-bonusPoinst = bonusPoints + 100;
-console.log(bonusPoints);
-bonusPoints = bonusPoints - 25;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 70;
-console.log(bonusPoints);
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+let count = 0;
+function increment() {
+  count = count + 1;
+  console.log(count);
+}
