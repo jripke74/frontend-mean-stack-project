1da913929678dce6b5b63225f6aa9b6a811669c9
Jeff Ripke <jripke74@gmail.com>
2024-09-24 13:42:33 -0700

    add bonusPoints

--- misc/index.js
+++ misc/index.js
@@ -1,9 +1,16 @@
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge
-// 3. Log myDogAge to the console
+let count = 5;
+count = count + 1;
+console.log(count);
 
-let myAge = 50;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
 
-console.log(myDogAge);
+let bonusPoints = 50;
+console.log(bonusPoints);
+bonusPoinst = bonusPoints + 100;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 25;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 70;
+console.log(bonusPoints);
