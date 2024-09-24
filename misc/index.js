1759de9f6a72a5bfafa3f0e3c50a07a1930ccab7
Jeff Ripke <jripke74@gmail.com>
2024-09-24 13:05:00 -0700

    add myDogAge

--- misc/index.js
+++ misc/index.js
@@ -1,6 +1,9 @@
-let firstBatch = 5;
-let secondBatch = 7;
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
 
-let count = firstBatch + secondBatch;
+let myAge = 50;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
 
-console.log(count);
+console.log(myDogAge);
