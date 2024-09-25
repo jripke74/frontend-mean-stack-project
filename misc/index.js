aae7f5b3d7f2d1dedf64c8939019620fa85a5263
Jeff Ripke <jripke74@gmail.com>
2024-09-25 10:52:51 -0700

    add save()

--- misc/index.js
+++ misc/index.js
@@ -1,12 +1,14 @@
-let lapsCompleted = 0;
+// grab the count-el element, store it in a countEl variable
+let countEl = document.getElementById("count-el");
+let count = 0;
 
-// Create a function that increments the lapsCompleted variable with one
-// Run it three times
-
-function incrementLaps() {
-  lapsCompleted = lapsCompleted + 1;
+function increment() {
+  count = count + 1;
+  // set countEl's innerText to the count
+  countEl.innerText = count;
 }
-incrementLaps();
-incrementLaps();
-incrementLaps();
-console.log(lapsCompleted);
+
+// 1. Create a function, save(), which logs out the count when it's btn is pressed
+function save() {
+  console.log(count);
+}
