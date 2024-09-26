b471892ac9bc898ea3b85e9e535675e9627f9ca7
Jeff Ripke <jripke74@gmail.com>
2024-09-26 13:18:58 -0700

    add welcomeEl

--- misc/index.js
+++ misc/index.js
@@ -1,14 +1,10 @@
-// grab the count-el element, store it in a countEl variable
-let countEl = document.getElementById("count-el");
-let count = 0;
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el");
 
-function increment() {
-  count = count + 1;
-  // set countEl's innerText to the count
-  countEl.innerText = count;
-}
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+let name = "Jeff Ripke";
+let greeting = "Welcome back ";
 
-// 1. Create a function, save(), which logs out the count when it's btn is pressed
-function save() {
-  console.log(count);
-}
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + name;
