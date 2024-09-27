a158d3db80d0db413d8210b5e3fc5913864507b7
Jeff Ripke <jripke74@gmail.com>
2024-09-27 10:39:50 -0700

    add error msg

--- misc/index.js
+++ misc/index.js
@@ -1,15 +1,22 @@
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
-let count = 0;
+// let saveEl = document.getElementById("save-el");
+// let countEl = document.getElementById("count-el");
+// let count = 0;
 
-function increment() {
-  count += 1;
-  countEl.textContent = count;
-}
+// function increment() {
+//   count += 1;
+//   countEl.textContent = count;
+// }
 
-function save() {
-  let countStr = count + " - ";
-  saveEl.textContent += countStr;
-  count = 0;
-  countEl.textContent = 0;
+// function save() {
+//   let countStr = count + " - ";
+//   saveEl.textContent += countStr;
+//   count = 0;
+//   countEl.textContent = 0;
+
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error"
+let errorMessage = document.getElementById("error");
+function purchase() {
+  errorMessage.textContent = "Something went wrong, please try again";
 }
