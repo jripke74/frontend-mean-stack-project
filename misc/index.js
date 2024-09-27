ac72bffb6a220c69075525b1089aebb566fffaf0
Jeff Ripke <jripke74@gmail.com>
2024-09-27 07:48:51 -0700

    change innerText to textContent

--- misc/index.js
+++ misc/index.js
@@ -5,14 +5,14 @@ let count = 0;
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
   // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
   let countStr = count + " - ";
   // 3. Render the variable in the saveEl using innerText
-  saveEl.innerText += countStr;
+  saveEl.textContent += countStr;
   // NB: Make sure to not delete the existing content of the paragraph
   console.log(count);
 }
