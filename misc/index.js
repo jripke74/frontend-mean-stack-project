259a5d12ca3660d5b9bc46dca2e85ea62da2bb26
Jeff Ripke <jripke74@gmail.com>
2024-10-03 07:38:41 -0700

    add message

--- misc/index.js
+++ misc/index.js
@@ -1,17 +1,21 @@
 let firstCard = 10;
-let secondCard = 12;
+let secondCard = 9;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+// 1. Declare a variable called message and assign its value to an empty string
+let message = "";
 
-// Write the conditional according to thse rules;
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack!"
-// else -> "You're out of the game! 😭"
-
+// 2. Reassign the message variable to the string we're logging out
 if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂");
+  message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack!");
+  message = "Wohoo! You've got Blackjack!";
+  hasBlackJack = true;
 } else {
-  console.log("You're out of the game! 😭");
+  message = "You're out of the game! 😭";
+  isAlive = false;
 }
+
+// 3. Log it out!
+console.log(message);
